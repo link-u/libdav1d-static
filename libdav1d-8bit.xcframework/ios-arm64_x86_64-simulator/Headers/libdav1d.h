f297c9a323a388b9c9bd2c0c5bb208540a8e1c51
murakami <zonaryfund@gmail.com>
2021-05-26 19:37:39 +0900

    built 8/16 bit only binaries

---
+++ libdav1d-8bit.xcframework/ios-arm64_x86_64-simulator/Headers/libdav1d.h
@@ -0,0 +1,18 @@
+//
+//  libdav1d.h
+//  libdav1d
+//
+//  Created by murakami on 2021/05/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libdav1d.
+FOUNDATION_EXPORT double libdav1dVersionNumber;
+
+//! Project version string for libdav1d.
+FOUNDATION_EXPORT const unsigned char libdav1dVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libdav1d/PublicHeader.h>
+
+#import <libdav1d/dav1d.h>
