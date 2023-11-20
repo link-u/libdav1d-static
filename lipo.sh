9442d66ea783a3ed5a6d5a09c9b0bedce966f317
y-hashida <yoshimasa.hashida@link-u.co.jp>
2023-11-21 01:50:36 +0900

    zip で固めるところまで用意

--- lipo.sh
+++ lipo.sh
@@ -1,16 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 #  build.sh
 #  libdav1d
 #
 #  Created by murakami on 2021/05/14.
-#  
+#
 
-cd generated
-mkdir iphone
-mkdir simulator
+set -eu
+set -o pipefail
+
+## スクリプトの保存されているディレクトリに移動する
+SCRIPTS_DIR="$(cd $(dirname "${0}") && pwd)/"
+
+cd "${SCRIPTS_DIR}/generated"
+mkdir -p iphone simulator
 
 lipo -create -output iphone/libdav1d.a -arch armv7 armv7-iphoneos.a -arch armv7s armv7s-iphoneos.a -arch arm64 arm64-iphoneos.a
 lipo -create -output simulator/libdav1d.a -arch arm64 arm64-iphone-simulator.a -arch x86_64 x86_64-iphone-simulator.a -arch i386 i386-iphone-simulator.a
-cd ../
+
+## ライブラリをビルド
+cd "${SCRIPTS_DIR}"
+rm -rf libdav1d.xcframework
 xcodebuild -create-xcframework -library generated/iphone/libdav1d.a -headers "headers" -library generated/simulator/libdav1d.a -headers "headers" -output libdav1d.xcframework
+
+## zip で固める
+zip libdav1d.xcframework.zip -r libdav1d.xcframework
